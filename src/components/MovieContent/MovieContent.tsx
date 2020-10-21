b2161d366e92e579bc5d1b5ae72020b417bf5b9f
Luiz Daniel Lima <luizdml@gmail.com>
2020-10-21 17:52:58 -0300

    finished separating MovieContent stuff into MovieInfo component

--- src/components/MovieContent/MovieContent.tsx
+++ src/components/MovieContent/MovieContent.tsx
@@ -1,10 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
 import {
-  IonChip,
   IonIcon,
   IonImg,
-  IonLabel,
   IonModal,
 } from '@ionic/react';
 import { connect } from 'react-redux';
@@ -28,8 +25,6 @@ const MovieContent = (props: any) => {
   const [windowWidth, setWindowWidth] = useState(0);
   const [windowHeight, setWindowHeight] = useState(0);
 
-  const history = useHistory();
-
   let resizeWindow = () => {
     setWindowWidth(window.innerWidth);
     setWindowHeight(window.innerHeight);
@@ -46,22 +41,6 @@ const MovieContent = (props: any) => {
   const baseURL = configuration.images.secure_base_url;
   const youtubeURL = 'https://www.youtube.com/embed/';
   const youtubeParams = '?showinfo=0&modestbranding=1';
-
-  const getMovieDuration = (totalMin: number) => {
-    const hours = totalMin / 60;
-    const rhours = Math.floor(hours);
-    const minutes = (hours - rhours) * 60;
-    const rminutes = Math.round(minutes);
-    if (rminutes === 0) {
-      return rhours + ' h';
-    } else {
-      return rhours + ' h  ' + rminutes + ' min';
-    }
-  };
-
-  const getMovieRatingPct = (movie: any) => {
-    return movie.vote_average * 10 + '%';
-  };
 
   const setTrailerIDAndOpenModal = (id: string) => {
     setTrailerID(id);
@@ -85,11 +64,6 @@ const MovieContent = (props: any) => {
     setShowModal(true);
   };
 
-  const changeGenreAndNavToMovies = (genreID: string) => {
-    props.onSearchParamChanged('genre', genreID);
-    history.push('/movies/');
-  };
-
   let {
     movie,
     showMode,
@@ -99,41 +73,8 @@ const MovieContent = (props: any) => {
     posters,
     trailers,
   } = props;
-  const movieGenres = movie.genres;
   let movieContent: any;
   movieContent = <p>Loading movie´s info</p>;
-
-  let crewList;
-  crewList = <p>Loading crew...</p>;
-  if (crew) {
-    crewList = crew.slice(0, 12).map((person: any, index: number) => {
-      return (
-        <div key={index} className="crew-item">
-          <p>{person.name}</p>
-          <p>......</p>
-          <p>{person.job}</p>
-        </div>
-      );
-    });
-  }
-
-  let genres;
-  if (movieGenres) {
-    genres = movieGenres.map((genre: any) => {
-      return (
-        <IonChip
-          className="chip"
-          key={genre.id}
-          outline
-          onClick={() =>
-            changeGenreAndNavToMovies(genre.id.toString())
-          }
-        >
-          <IonLabel>{genre.name}</IonLabel>
-        </IonChip>
-      );
-    });
-  }
 
   let moviePosters;
   if (posters) {
@@ -198,69 +139,9 @@ const MovieContent = (props: any) => {
 
   if (movie) {
     if (showMode === 'main') {
-      movieContent = <MovieInfo movie={movie} crew={crew}/>
-      // movieContent = (
-      //   <div
-      //     className="main-container"
-      //     style={{
-      //       background: `linear-gradient(rgba(146, 213, 230, 0.9), rgba(128, 187, 202, 0.9)) no-repeat center top / cover,
-      //       url("${baseURL}${posterSizes[5]}${movie.poster_path}") no-repeat center center / cover`,
-      //     }}
-      //   >
-      //     <div className="main-wrapper">
-      //       <div className="main-info">
-      //         <picture className="thumb-container">
-      //           <IonImg
-      //             className="thumb"
-      //             src={`${baseURL}${posterSizes[3]}${movie.poster_path}`}
-      //             alt="movie poster"
-      //           />
-      //         </picture>
-      //         <div className="movie-stats">
-      //           <div className="movie-stat-item">
-      //             <p className="movie-stat-title">
-      //               Rating {movie.vote_average}
-      //             </p>
-      //             <div className="rating rating-centered">
-      //               <span
-      //                 style={{ width: getMovieRatingPct(movie) }}
-      //               ></span>
-      //             </div>
-      //           </div>
-      //           <div className="movie-stat-item">
-      //             <p className="movie-stat-title">Runtime</p>
-      //             <h6 className="movie-stat-value">
-      //               {getMovieDuration(movie.runtime)}
-      //             </h6>
-      //           </div>
-      //           <div className="movie-stat-item">
-      //             <p className="movie-stat-title">Budget</p>
-      //             <h6 className="movie-stat-value">
-      //               $ {movie.budget}
-      //             </h6>
-      //           </div>
-      //           <div className="movie-stat-item">
-      //             <p className="movie-stat-title">Revenue</p>
-      //             <h6 className="movie-stat-value">
-      //               $ {movie.revenue}
-      //             </h6>
-      //           </div>
-      //         </div>
-      //       </div>
-      //       <div className="secondary-info">
-      //         <div className="genres">{genres}</div>
-      //         <div className="overview">
-      //           <h4>Overview</h4>
-      //           <p>{movie.overview}</p>
-      //         </div>
-      //         <div className="crew">
-      //           <h4>Featured Crew</h4>
-      //           {crewList}
-      //         </div>
-      //       </div>
-      //     </div>
-      //   </div>
-      // );
+      movieContent = (
+        <MovieInfo movie={movie} crew={crew}/>
+      )
     } else if (showMode === 'cast') {
       movieContent = (
         <div className="actorlist-container">
